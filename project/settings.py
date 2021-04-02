39910b522143f901cea7656410f56e5dc22d49ce
zhengbigbig <780357902@qq.com>
2021-04-02 16:49:41 +0800

    跨域配置

--- project/settings.py
+++ project/settings.py
@@ -26,12 +26,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf',
     'django_filters',
+    'corsheaders',
 ]
 # 中间件
 MIDDLEWARE = [
     # 'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -40,6 +42,7 @@ MIDDLEWARE = [
 
     'middleware.myApp.myMiddle.MyMiddle',
     # 'django.middleware.cache.FetchFromCacheMiddleware',
+
 ]
 # 根路由
 ROOT_URLCONF = 'project.urls'
@@ -321,3 +324,31 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
+
+# 处理跨域问题
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'http://10.0.122.*',
+    'http://localhost'
+)
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+CORS_ALLOW_HEADERS = (
+    'XMLHttpRequest',
+    'X_FILENAME',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
